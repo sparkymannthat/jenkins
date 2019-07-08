6d1735842c0ee270035e62d7885664f24e192be5
Simon Parkinson <simon@Simons-MBP.home>
2019-07-08 07:13:23 +0100

    Added base files from macbook

---
+++ jenkins.tf
@@ -0,0 +1,21 @@
+# Create a resource group if it doesn�t exist
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = "myResourceGroup"
+    location = "eastus"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
